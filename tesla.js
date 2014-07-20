0770001250ccfbfac304cdae59a062c701d1d13c
Prajit Ramachandran <ramprajit@gmail.com>
2014-07-20 10:31:25 -0700

    console.log to check if it parses the result correctly.

--- tesla.js
+++ tesla.js
@@ -7,6 +7,7 @@ function getInitialCharge() {
   for (var i = body.length - 1; i >= 0; i--) { // Pick out valid part of file.
     if (body[i].indexOf('battery_level:') != -1) { // Pick out the last object.
       initialCharge = +(body[i].split(':')[1].slice(0, -1));
+      console.log(initialCharge);
       return initialCharge;
     }
   }
